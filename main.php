55e6be495da66040a5e14f72900e8c42b9c76f6f
Nik Topler <nik.topler21@gmail.com>
2021-02-14 19:19:07 +0100

    Structure setup

---
+++ main.php
@@ -0,0 +1,19 @@
+<?php
+
+class webScraping {
+
+    public function setup($website) {
+    }
+
+    public function getLinks($xpath) {
+    }
+
+    public function loopThroughLinks() {
+    }    
+
+    public function fetchDataFromLinks($l) {
+    }
+}
+
+
+
